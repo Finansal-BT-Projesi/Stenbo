e5a34b231175cd9d07106cdeeccc8fd0260fe5cd
fatmanurBulacak <30408581+fatmanurBulacak@users.noreply.github.com>
2017-10-16 16:44:21 +0300

    Activityler oluşturuldu

---
+++ app/src/main/java/com/finansal/bt/project/StartActivity.java
@@ -0,0 +1,31 @@
+package com.finansal.bt.project;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(StartActivity.this,LevelActivity.class);
+                startActivity(i);
+
+
+            }
+        },1000);
+
+
+
+    }
+}
